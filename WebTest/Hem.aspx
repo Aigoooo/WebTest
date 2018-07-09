9786e30cd1dcce1dd467e8a030cd3cfe4d09d83b
Aigoooo <Undead_60@hotmail.com>
2018-07-09 10:44:17 +0200

    Importing project

---
+++ WebTest/Hem.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Hem.aspx.cs" Inherits="WebTest.Hem" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        Namn<asp:TextBox ID="txtNamn" runat="server"></asp:TextBox>
+        <p>
+            Ålder<asp:TextBox ID="txtÅlder" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Smeknamn<asp:TextBox ID="txtSmeknamn" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="btnSpara" runat="server" OnClick="Button1_Click" Text="Spara" />
+        <asp:Button ID="btnRadera" runat="server" Text="Radera" />
+        <asp:Button ID="btnAvbry" runat="server" Text="Avbryt" />
+        <asp:Label ID="LblJa" runat="server" Text="Label" ForeColor="Green" Visible="False"></asp:Label>
+        <asp:Label ID="LblNej" runat="server" Text="Label" ForeColor="Red" Visible="False"></asp:Label>
+        <asp:GridView ID="grdData" runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="Namn" HeaderText="Namn" />
+                <asp:BoundField DataField="Ålder" HeaderText="Ålder" />
+                <asp:BoundField DataField="Smeknamn" HeaderText="Smeknamn" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <%--<asp:LinkButton ID="lnkView" runat="server" CommandArgument='<%# Eval("Id")%>'>View</asp:LinkButton>--%>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+
+            </Columns>
+
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebTest/Hem.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebTest
+{
+    public partial class Hem : System.Web.UI.Page
+    {
+        PersonalEntities _db = new PersonalEntities();
+        Personal personal = new Personal();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Clear();
+            ShowData();
+            if (!IsPostBack)
+            {
+                btnRadera.Enabled = false;
+            }
+        }
+        void Clear()
+        {
+            txtNamn.Text = txtÅlder.Text = txtSmeknamn.Text;
+            btnSpara.Text = "Save";
+            LblJa.Text = LblNej.Text = "";
+            btnRadera.Enabled = false;
+            personal.Id = 0;
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            AddData();
+            
+        }
+        private void ShowData()
+        {
+            using (PersonalEntities _db = new PersonalEntities())
+            {
+
+                var showData = from p in _db.Personals
+                               select new { p.Namn, p.Ålder, p.Smeknamn };
+
+                grdData.DataSource = showData.ToList();
+                grdData.DataBind();
+                    
+            }
+        }
+        private void AddData()
+        {
+                personal.Namn = txtNamn.Text.Trim();
+                personal.Ålder = Convert.ToInt32(txtÅlder.Text.Trim());
+           
+            personal.Smeknamn = txtSmeknamn.Text.Trim();
+            using (PersonalEntities _db = new PersonalEntities())
+            {
+                if (personal.Id == 0)
+                {
+                    _db.Personals.Add(personal);
+                }
+                else
+                {
+                    _db.Entry(personal).State = System.Data.Entity.EntityState.Modified;
+                }
+
+                
+                _db.SaveChanges();
+                Clear();
+                ShowData();
+                //_db.Personals.Add(personal);
+                //_db.SaveChanges();
+            }
+        }
+    }
+}
